d83088bc096373148072b62ed8d0388ef0fc5d0b
Dory <dorybaga@gmail.com>
2017-07-29 15:28:40 -1000

    added mural data

---
+++ data.js
@@ -0,0 +1,37 @@
+[
+
+{"placeId": "1",
+"artistName": "OG Slick",
+"artistURL": "https://www.instagram.com/og_slick/?hl=en",
+"muralImg": "http://powwowhawaii.com/wp-content/uploads/2017/03/pwh2017_OGSlick01_BrandonShigeta.jpg",
+"muralName": "Big Slick Aloha",
+"latitute": "21.295967",
+"longitude": "-157.8590888"},
+
+{"placeId": "2",
+"artistName": "Kevin Lyons",
+"artistURL": "http://www.naturalborn.com/",
+"muralImg": "http://powwowhawaii.com/wp-content/uploads/2017/03/pwh2017_KevinLyons02_BrandonShigeta.jpg",
+"muralName": "Fluffy Monsters",
+"latitute": "21.2993957",
+"longitude": "-157.8591853"},
+
+{"placeId": "3",
+"artistName": "808 Urban",
+"artistURL": "http://808urban.org/",
+"muralImg": "http://powwowhawaii.com/wp-content/uploads/2017/03/pwh2017_Prime_BrandonShigeta.jpg",
+"muralName": "Prime",
+"latitute": "21.2970166",
+"longitude": "-157.8599900"},
+
+{"placeId": "4",
+"artistName": "Shok-1",
+"artistURL": "https://store.shok1.com/",
+"muralImg": "http://powwowhawaii.com/wp-content/uploads/2017/03/pwh2017_Shok1_BrandonShigeta.jpg",
+"muralName": "Shaka X-Ray",
+"latitute": "21.2975939",
+"longitude": "-157.8604098"}
+
+]
+
+
